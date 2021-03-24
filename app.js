5a2b2684853a1de0b4b57dd04cbd90e2c5281142
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-03-23 20:49:34 -0500

    saludo2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app= express();
 
-app.get('/',(req, res)=>{
-    res.send(`<h1>Hola ${req.query.nombre || "desconocido"}!</h1>`);
+app.get('/makers/:nombre',(req, res)=>{
+    res.send(`<h1>Hola ${req.params.nombre || "desconocido"}!!</h1>`);
 });
 
 app.listen(3000, () => console.log('Linstening on port 3000!'));
