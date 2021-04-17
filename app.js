7dce3fd1e75c7696759bea8f4f4fd6e684e58851
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-04-16 19:26:49 -0500

    saludame3

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app= express();
 app.set('view engine', 'pug');
 app.set('views', 'views');
+app.use(express.urlencoded());
+
+
 
 // app.get('/',(req, res)=>{
 //     let ens= ['1 soy impar','2 soy par']
@@ -10,6 +13,9 @@ app.set('views', 'views');
 
 app.get('/', (req, res) => {
     res.render('index');
-  });
-  
+});
+
+app.post('/hello',(req,res)=>{
+  res.send("Hola "+req.body.name)
+})
 app.listen(3000, () => console.log('Linstening on port 3000!'));
