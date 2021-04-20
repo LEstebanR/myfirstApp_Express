6a01de2c51dd0d354368712ccd2fa9346a298011
Luis Esteban Ramirez <leramirezca@gmail.com>
2021-04-19 23:20:43 -0500

    visitantes

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app= express();
-app.set('view engine', 'pug');
-app.set('views', 'views');
-app.use(express.urlencoded());
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+// app.use(express.urlencoded());
+const mongoose=require("mongoose");
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connection.on{"error", function(e){console.error(e);}};
+
+const visitorSchema = mongoose.Schema({
+  date:{type: date, default: Date.now},
+  name:{type:string, default: "Anónimo"},
+})
+
+const Visitor = mongoose.model("Visitor", visitorSchema);
+
+app.get('/', (req, res) => {
+  const name = req.query.name;
+  Visitor.create({...(name ? {name}:{})}, function(err){
+    if (err) return console.error(err);
+    res.send('<h1>El visitante fue almacenado con éxito</h1>')
+
+  })
+});
+
+
+app.listen(3000, () => console.log('Linstening on port 3000!'));
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -11,11 +43,10 @@ app.use(express.urlencoded());
 //     res.send(`<p>${ens}!</p>`);
 // });
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
 
-app.post('/hello',(req,res)=>{
-  res.send("<h1>Hola "+req.body.name+"!</h1>")
-})
-app.listen(3000, () => console.log('Linstening on port 3000!'));
+// app.post('/hello',(req,res)=>{
+//   res.send("<h1>Hola "+req.body.name+"!</h1>")
+// })
