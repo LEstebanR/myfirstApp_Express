57e37b8f57140a22622818167a8a3db45393d06e
Luis Esteban Ramirez <leramirezca@gmail.com>
2021-05-04 20:41:40 -0500

    devolviendo json

--- app.js
+++ app.js
@@ -4,34 +4,45 @@ const mongoose=require("mongoose");
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology:true });
 mongoose.connection.on("error", function(e){console.error(e);});
 
-var UserSchema= new mongoose.Schema({
+var products = new mongoose.Schema({
   name:{type:String},
-  email: {type: String},
-  password: {type: String}
+  price: {type: Number},
 })
 
-var User = mongoose.model("User", UserSchema);
+app.get('/products', async (req, res) => {
+  await products.create({name,price})
+  const products = await products.find({})
+  res.json(books)
+})
+
+// var UserSchema= new mongoose.Schema({
+//   name:{type:String},
+//   email: {type: String},
+//   password: {type: String}
+// })
+
+// var User = mongoose.model("User", UserSchema);
 
 
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
-app.get('/register', async (req, res) => {
-  res.render('register')
+// app.get('/register', async (req, res) => {
+//   res.render('register')
   
-});
+// });
 
-app.post('/register', async (req,res)=>{
-  await User.create({name:req.body.name, email:req.body.email}).catch(error=>{console.error(error)})
-  const users = await User.find();
-  res.render('data',{User:user}).catch(error=>{console.error(error)})})
+// app.post('/register', async (req,res)=>{
+//   await User.create({name:req.body.name, email:req.body.email}).catch(error=>{console.error(error)})
+//   const users = await User.find();
+//   res.render('data',{User:user}).catch(error=>{console.error(error)})})
 
 
 
